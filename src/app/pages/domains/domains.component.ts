927c669dafd207c41757975ac3ad893cf43808a9
BarrensChat <jeremy.zuccolin@gmail.com>
2020-04-13 23:05:38 -0400

    color file created and imported

--- src/app/pages/domains/domains.component.ts
+++ src/app/pages/domains/domains.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faPiggyBank } from '@fortawesome/free-solid-svg-icons';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-domains',
@@ -8,6 +10,8 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 export class DomainsComponent implements OnInit {
   faCoffee = faCoffee;
+  faPiggyBank = faPiggyBank;
+  faDollarSign = faDollarSign;
 
   constructor() {
     
