d7e439c7f1dbe1d55d53e2c84e8c6bfadacda180
LAPTOP-7N3VIO1R\Jerry Jinkies <jajabinks@hotmail.com>
2022-03-21 00:35:09 -0400

    Prepping large overhaul and best practices implementation with Jasmine and Karma.

--- src/app/main-nav/main-nav.module.ts
+++ src/app/main-nav/main-nav.module.ts
@@ -3,24 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatRippleModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSliderModule } from '@angular/material/slider';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MainNavRoutingModule } from './main-nav-routing.module';
 import { AngularHelperLinksComponent } from './angular-helper-links/angular-helper-links.component';
-import { CarouselComponent } from '../common-components/carousel/carousel.component';
+import { CarouselComponent } from '../modules/common-components-module/carousel/carousel.component';
 
 
 
@@ -34,20 +21,6 @@ import { CarouselComponent } from '../common-components/carousel/carousel.compon
     CommonModule,
     NgbModule,
     FontAwesomeModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatSliderModule,
-    MatChipsModule,
-    MatRippleModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    MatMenuModule
   ]
 })
 export class MainNavModule { }
---
+++ src/app/modules/angular-material-module/angular-material-module.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSliderModule } from '@angular/material/slider';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatSliderModule,
+    MatChipsModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatMenuModule
+  ]
+})
+export class AngularMaterialModuleModule { }
